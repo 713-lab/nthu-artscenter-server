fe67a5e8cd71727e549aedc0c7a852ae8f14adc6
106062209 <swiftfo142857@outlook.com>
2020-03-23 00:15:52 +0800

    test

--- test/media.test.ts
+++ test/media.test.ts
@@ -8,8 +8,8 @@ console.log(process.env.DB_PASSWORD);
 import app from "../src/app";
 
 describe("GET /api", () => {
-  test("should return 200 OK", async () => {
-    return await request(app).get("/api/v2/medias")
+  test("should return 200 OK", () => {
+    return request(app).get("/api/v2/medias")
           .expect(200);
   })
 })
