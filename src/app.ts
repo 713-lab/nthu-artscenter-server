422c38041e50c25929fe6eb12b65fa4e9633af96
106062209 <swiftfo142857@outlook.com>
2020-03-23 09:39:45 +0800

    test

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from "dotenv";
 import routeV2 from './routes/route.v2';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import { startDataBase } from './models/index';
 import { db } from './config/database'
+import morganBody from 'morgan-body';
 
 
 dotenv.config();
@@ -13,16 +14,24 @@ const app = express();
 
 db.sync()
 .then(async() => {
-  await startDataBase();
+  try {
+    await startDataBase();
+    // tslint:disable-next-line:no-console
+    console.log("Database OK")
+  } catch(err) {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  }
+
 })
 
 app.set("port", process.env.SERVER_PORT || 8080);
 
 app.use(bodyParser.json());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+morganBody(app);
 
 app.use('/api/v2', routeV2);
 
