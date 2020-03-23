3a7a11c7ff4b2cfbdda02174bff05b28b1a4e653
106062209 <swiftfo142857@outlook.com>
2020-03-23 15:20:55 +0800

    edit

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,9 @@ import { Media } from "./models/Media";
 
 app.listen(app.get("port"), async () => {
   try {
-    // tslint:disable-next-line:no-console
     await Exhibition.sync({alter: true})
     await Media.sync({alter: true});
+    // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ app.get("port") }` );
   } catch(err) {
     // tslint:disable-next-line:no-console
