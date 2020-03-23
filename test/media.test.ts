27518688ca0d342dd0fada822250857b1ad55aab
106062209 <swiftfo142857@outlook.com>
2020-03-23 12:37:49 +0800

    123

--- test/media.test.ts
+++ test/media.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import app from "../src/app";
 
+// tslint:disable-next-line:no-console
+console.log(`${process.env.DB_NAME}\n${process.env.DB_USERNAME}\n${process.env.DB_PASSWORD}\n${process.env.SERVER_PORT}}`)
 
 describe("GET /api", () => {
   it("should return 200 OK", () => {
